bf2d325cb2449def365f5109240bd7c46705b78f
Natasha Nicole Navarro Nolasco <nicolenolas24@gmail.com>
2024-09-20 20:34:02 -0600

    se creo GetIdResultAuto

---
+++ Autos.DTOs/Autos.DTOs/GetIdResultAuto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autos.DTOs.Autos.DTOs
+{
+    internal class GetIdResultAuto
+    {
+        public int Id { get; set; }
+       
+        [Display(Name = "Marca")]
+        public string Marca { get; set; }
+
+        [Display(Name = "Modelo")]
+        public string Modelo { get; set; }
+
+        [Display(Name = "Año")]
+        public int Year { get; set; }
+
+        [Display(Name = "Precio")]
+        public decimal Precio { get; set; }
+    }
+}
